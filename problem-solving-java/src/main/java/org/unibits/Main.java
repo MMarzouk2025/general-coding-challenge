7279a388290837f1ea41472307a1de13ea6aef70
mKhafaga <mahmoud.khafaga@shop-apotheke.com>
2023-10-01 03:52:52 +0200

    Pr-1768: MergeStringsAlternately

--- problem-solving-java/src/main/java/org/unibits/Main.java
+++ problem-solving-java/src/main/java/org/unibits/Main.java
@@ -1,13 +1,7 @@
 package org.unibits;
 
-import org.unibits.leetcode.programming_skills.MergeStringsAlternatelyPr1768;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println(
-                new MergeStringsAlternatelyPr1768().mergeAlternately("a", "p")
-        );
     }
 }
--- problem-solving-java/src/main/java/org/unibits/leetcode/programming_skills/MergeStringsAlternatelyPr1768.java
+++ problem-solving-java/src/main/java/org/unibits/leetcode/programming_skills/MergeStringsAlternatelyPr1768.java
@@ -3,15 +3,20 @@ package org.unibits.leetcode.programming_skills;
 public class MergeStringsAlternatelyPr1768 {
 
     public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int maxLength = Math.max(m, n);
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < Math.max(word1.length(), word2.length()); i++) {
-            if (i < word1.length()) {
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < m) {
                 result.append(word1.charAt(i));
             }
-            if (i < word2.length()) {
+            if (i < n) {
                 result.append(word2.charAt(i));
             }
         }
+
         return result.toString();
     }
 
